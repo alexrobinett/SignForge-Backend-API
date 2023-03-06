459ecaa4f33fec648f9ef7aef21606f571734b6c
robinetta <alxr19@gmail.com>
2023-03-05 23:54:45 -0500

    cors and vercel json

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const cors = require('cors')
 
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db = mongoose.connection
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors)
 
 app.use('/', indexRouter);
 app.use('/messages', messagesRouter);
