3e816884e72f33b2f0c482a355b167478f94bbd3
robinetta <alxr19@gmail.com>
2023-03-06 00:17:29 -0500

    home update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("welcome to the signage API");
 });
 
 module.exports = router;
