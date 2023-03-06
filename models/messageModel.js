80de23f8e4567c710d4889652363346148827bb5
robinetta <alxr19@gmail.com>
2023-03-05 23:46:03 -0500

    initial Comit with CRD

---
+++ models/messageModel.js
@@ -0,0 +1,60 @@
+// Require Mongoose
+const mongoose = require("mongoose");
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+const storeMessageModelSchema = new Schema({
+    imageOne: {
+        type: String,
+        required: true
+    },
+    imageTwo: {
+        type: String,
+        required: true
+    },
+    imageThree:{
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: String,
+        required: true
+    },
+    points:{
+        type: String,
+        required: true
+    },
+    promo:{
+        type: String,
+        required: true
+    },
+    promoLineOne:{
+        type: String,
+        required: true
+    },
+    promoLineTwo: {
+        type: String,
+        required: true
+    },
+    disclaimerLineOne: {
+        type: String,
+        required: true
+    },
+    disclaimerLineTwo:{
+        type: String,
+        required: true
+    },
+    dateCreated:{
+        type: Date,
+        default: Date.now,
+        required: true,
+    }
+});
+
+// Export function to create "SomeModel" model class
+module.exports = mongoose.model("StoreMessageModel", storeMessageModelSchema);
---
+++ routes/messages.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const StoreMessage = require('../models/messageModel')
+
+/* GET users listing. */
+router.get('/', async (req, res, next) => {
+  try{
+    const messages = await StoreMessage.find()
+    res.json(messages)
+  }catch(error) {
+    res.status(500).json({message: error.message})
+  }
+
+});
+
+router.post('/', async (req,res,next)=> {
+  const message = new StoreMessage({
+    imageOne: req.body.imageOne,
+    imageTwo: req.body.imageTwo,
+    imageThree: req.body.imageThree,
+    price: req.body.price,
+    quantity: req.body.quantity,
+    points: req.body.points,
+    promo: req.body.promo,
+    promoLineOne: req.body.promoLineOne,
+    promoLineTwo: req.body.promoLineTwo,
+    disclaimerLineOne: req.body.disclaimerLineOne,
+    disclaimerLineTwo: req.body.disclaimerLineTwo,
+  })
+  try{
+    const newMessage = await message.save()
+    res.status(201).json(newMessage)
+  } catch (error){
+    res.status(400).json({message: error.message})
+  }
+})
+
+router.get('/:id', getStoreMessage ,function(req, res, next) {
+  res.json(res.adSpotMessage)
+});
+
+
+router.delete('/:id', getStoreMessage, async (req,res) =>{
+  spotMessage = res.adSpotMessage
+  try {
+    await spotMessage.deleteOne()
+    res.json({Message: "deleted message!"})
+  } catch (error) {
+    res.status(500).json({Message: error.message})
+  }
+})
+
+
+async function getStoreMessage(req,res, next){
+  let message
+
+  try{
+    message = await StoreMessage.findById(req.params.id)
+    if(message == null){
+      return res.status(404).json({Message: 'Cannot Find Message'})
+    }
+  } catch(error){
+    return res.status(500).json({Message: error.message} )
+  }
+  res.adSpotMessage = message
+  next()
+}
+
+module.exports = router;
