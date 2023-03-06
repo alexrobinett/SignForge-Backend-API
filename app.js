c761a3d486c21b83a6c9f02d2d4674d385e481a5
robinetta <alxr19@gmail.com>
2023-03-06 00:03:13 -0500

    cors fix

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors)
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/messages', messagesRouter);
