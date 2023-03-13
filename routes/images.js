6d08a5c52f63ff3016de5c7534226b7ec5da4a37
robinetta <alxr19@gmail.com>
2023-03-12 21:11:52 -0400

    file rename deploy

--- routes/images.js
+++ routes/images.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const imageController = require('../controllers/imageController');
+const imageController = require('./controllers/imageController');
 
 router
   .route('/')
