2a6ca26b3c9685744f912a1f5744acc34733cdfd
robinetta <alxr19@gmail.com>
2023-04-19 23:51:42 -0400

    auth updates for JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ const login = asyncHandler(async (req, res) => {
             }
         },
         process.env.ACCESS_TOKEN_SECRET,
-        {expiresIn: '15m'}
+        {expiresIn: '30m'}
     )
 
     const refreshToken = jwt.sign(
